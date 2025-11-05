8151ae0e5b59271d2e9c4645490ccbe957a45b24
Jay <jjolajitan2006@gmail.com>
2025-11-05 14:29:39 +0000

    are you abe to make the scroll speed for both carousels a smidge faster?

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -107,8 +107,8 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'infinite-scroll': 'infinite-scroll 120s linear infinite',
-        'infinite-scroll-reverse': 'infinite-scroll-reverse 120s linear infinite',
+        'infinite-scroll': 'infinite-scroll 80s linear infinite',
+        'infinite-scroll-reverse': 'infinite-scroll-reverse 80s linear infinite',
       },
     },
   },
